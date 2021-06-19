3df6f56a3d3d7ef3dd9fb29e26c72d7058b023de
Ekaterina-11 <ermoshenko.ekaterina@yandex.ru>
2021-06-20 00:17:54 +0400

    username displaing

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,12 +1,19 @@
 package controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        Authentication currentUser = SecurityContextHolder.getContext().getAuthentication();
+        if(!currentUser.getName().equals("anonymousUser")){
+            model.addAttribute("currentUser", currentUser.getName());
+        }
         return "index";
     }
 }
