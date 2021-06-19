6436a4505387e05d540962102256e4e0fc4f0a38
Ekaterina-11 <ermoshenko.ekaterina@yandex.ru>
2021-06-19 19:53:43 +0400

    global security configuration

---
+++ src/main/java/configuration/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import services.AuthService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    AuthService authService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/login","/registration","/product/**","/activationProfile/**").permitAll()
+                    .antMatchers("/profile").hasAuthority("ROLE_CUSTOMER")
+                    .antMatchers("/administration","/admProduct","/admDelProduct","/admCustomer").hasAuthority("ROLE_ADMIN")
+                    .antMatchers("/","/*.css", "/*.js", "/img/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin().loginPage("/login").defaultSuccessUrl("/")
+                    .permitAll()
+                .and()
+                    .logout().logoutSuccessUrl("/")
+                    .permitAll();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authService).passwordEncoder(bCryptPasswordEncoder());
+    }
+}
